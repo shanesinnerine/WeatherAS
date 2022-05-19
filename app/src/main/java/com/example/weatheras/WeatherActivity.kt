08c4cf3fe261e30a40c76891abaf924819450017
Shane Sinnerine <shanesinnerine@gmail.com>
2022-05-19 19:02:00 -0400

    Created launch screen

--- app/src/main/java/com/example/weatheras/WeatherActivity.kt
+++ app/src/main/java/com/example/weatheras/WeatherActivity.kt
@@ -2,10 +2,21 @@ package com.example.weatheras
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_weather)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onClick(view: View) {
+        var txt = findViewById<TextView>(R.id.header)
+        var location = findViewById<EditText>(R.id.location)
+        txt.setText("You are in " + location.text.toString())
+        location.setText("")
+        txt.setTextSize(40F)
     }
 }
