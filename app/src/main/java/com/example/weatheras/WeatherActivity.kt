0076c863ebeed906df4cea9c86e7a4ca95cad7db
Shane Sinnerine <shanesinnerine@gmail.com>
2022-05-21 21:29:04 -0400

    made structural changes to support modularity for easy expansion of app

--- app/src/main/java/com/example/weatheras/WeatherActivity.kt
+++ app/src/main/java/com/example/weatheras/WeatherActivity.kt
@@ -25,32 +25,38 @@ class MainActivity : AppCompatActivity() {
         var API_KEY = "f75aa22ec6b143fd9843e47520f4274c"
         var location = findViewById<EditText>(R.id.location).text
         val queue = Volley.newRequestQueue(this)
-        //val url: String = "https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key="+API_KEY+"&include=minutely"
-        val url: String = "https://api.weatherbit.io/v2.0/current?" + "city=" + location.toString() + "&key=" + API_KEY
-        val stringReq = StringRequest(Request.Method.GET, url,
-            { response ->
-                Log.e("lat", response.toString())
-
+        val url: String = "https://api.weatherbit.io/v2.0/current?city=$location&key=$API_KEY"
+        val stringReq: StringRequest = makeStringReq(url, "city", txt)
+        queue.add(stringReq)
+                            //    { response ->
                 // get the JSON object
-                val obj = JSONObject(response)
+        //        val obj = JSONObject(response)
 
                 // get the Array from obj of name - "data"
-                val arr = obj.getJSONArray("data")
-                Log.e("lat obj1", arr.toString())
+        //        val arr = obj.getJSONArray("data")
 
                 // get the JSON object from the
                 // array at index position 0
-                val obj2 = arr.getJSONObject(0)
-                Log.e("lat obj2", obj2.toString())
+        //      val obj2 = arr.getJSONObject(0)
 
                 // set the temperature and the city
-                // name using getString() function
-                txt.text = obj2.getString("temp") + " deg Celsius in " + obj2.getString("city_name")
-            },
+        //        txt.setText(obj2.getString("temp") + " deg Celsius in " + obj2.getString("city_name"))
+        //    },
             // In case of any error
-            { txt!!.text = "That didn't work"})
-        queue.add(stringReq)
-        //val url: String = "http://api.weatherbit.io/v2.0/current" +"?city=" + location.text.toString() + "&key=f75aa22ec6b143fd9843e47520f4274c&include=minutely"
-
+        //    { txt!!.setText("That didn't work")})
+    }
+    fun makeStringReq(url: String, arguments: String, txt: TextView): StringRequest{
+        val stringReq = StringRequest(Request.Method.GET, url,
+            { response ->
+                if(arguments == "city") {
+                    val obj = JSONObject(response)
+                    val arr = obj.getJSONArray("data")
+                    val obj2 = arr.getJSONObject(0)
+                    txt.text = obj2.getString("temp") + " deg Celsius in " + obj2.getString("city_name")
+                } else {
+                    txt.text = "Not Found"
+                }
+        }, { txt.text = "That didn't work" })
+        return stringReq
     }
 }
