8050e901be17a8b76ff158bec0d0ec822dcd38da
omgitsAlec <alecwebley03@gmail.com>
2022-05-23 01:07:06 -0400

    Added a sliding transition, as well as an activity for the Longitude and Latitude

--- app/src/main/java/com/example/weatheras/CoordsActivity.kt
+++ app/src/main/java/com/example/weatheras/CoordsActivity.kt
@@ -1,22 +1,16 @@
 package com.example.weatheras
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import com.android.volley.Request
-import com.android.volley.toolbox.*
-import org.json.JSONObject
 
-class MainActivity : AppCompatActivity() {
+class CoordsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        findViewById<Button>(R.id.cityBtn).isSelected = true
+        setContentView(R.layout.activity_coords)
+        findViewById<Button>(R.id.coordBtn).isSelected = true
 
         findViewById<Button>(R.id.coordBtn).setOnClickListener {
             if (findViewById<Button>(R.id.cityBtn).isSelected) {
@@ -30,29 +24,28 @@ class MainActivity : AppCompatActivity() {
                 findViewById<Button>(R.id.cityBtn).isSelected = !findViewById<Button>(R.id.cityBtn).isSelected
             }
         }
-
+        val cityButton = findViewById<Button>(R.id.cityBtn)
+        cityButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+            //For coords mode: pass latitude, longitude, putExtra("mode", "coords")
+            //For city: pass city name, putExtra("mode", "city")
+        }
         val searchButton = findViewById<Button>(R.id.btn)
         searchButton.setOnClickListener {
-            var location = findViewById<EditText>(R.id.location).text.toString()
+            var longitude = findViewById<EditText>(R.id.longitude).text.toString()
+            var latitude = findViewById<EditText>(R.id.latitude).text.toString()
             val intent = Intent(this, SearchActivity::class.java).apply {
-                putExtra("location", location)
-                putExtra("mode", "city")
+                putExtra("longitude", longitude)
+                putExtra("latitude", latitude)
+                putExtra("mode", "coords")
             }
             //For coords mode: pass latitude, longitude, putExtra("mode", "coords")
             //For city: pass city name, putExtra("mode", "city")
             startActivity(intent)
             overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
         }
-        val coordButton = findViewById<Button>(R.id.coordBtn)
-        coordButton.setOnClickListener {
-            val intent2 = Intent(this, CoordsActivity::class.java)
-            startActivity(intent2)
-            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
-            //For coords mode: pass latitude, longitude, putExtra("mode", "coords")
-            //For city: pass city name, putExtra("mode", "city")
-        }
     }
 
-
 }
-
